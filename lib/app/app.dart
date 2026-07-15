a634905f4a49f70c180c6eb637feb78152663f24
kartturih <arttu.vihervuori@outlook.com>
2026-07-15 14:24:07 +0300

    feat: add initial application theme

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:fishing_app/app/router/app_router.dart';
+import 'package:fishing_app/app/theme/app_theme.dart';
 
 class FishingApp extends StatelessWidget {
   const FishingApp({super.key});
@@ -9,6 +10,7 @@ class FishingApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
+      theme: appTheme,
       routerConfig: appRouter,
     );
   }
---
+++ lib/app/theme/app_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+final ThemeData appTheme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: Colors.teal,
+  ),
+);
